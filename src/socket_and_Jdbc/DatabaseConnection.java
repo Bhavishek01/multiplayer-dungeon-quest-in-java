df59bfb9233c5e2ecef8f66fcdf8e32dd3946486
bhavishek01 <bhavisheklama01@gmail.com>
2025-09-04 14:51:39 +0545

    database added and connection done;

--- src/socket_and_Jdbc/DatabaseConnection.java
+++ src/socket_and_Jdbc/DatabaseConnection.java
@@ -9,17 +9,23 @@ public class DatabaseConnection {
     public static Connection getConnection()
     {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             String url="jdbc:mysql://localhost:3306/dungeon_quest_players";
             String user="root";
             String pass="";
-            //Class.forName("com.mysql.jdbc.Driver");
+            
             con = DriverManager.getConnection(url, user, pass);
+            System.out.println("connection established");
+
             }
         catch (ClassNotFoundException | SQLException e) 
             {
             e.printStackTrace();
             }
         return con; 
+    }
+
+    public static void main(String[] args) {
+        Connection con = DatabaseConnection.getConnection();
     }
 }
